423560d98f8106021a7a369124baac59f2e86ff3
Clifford <cliffordchristophermarx@gmail.com>
2025-11-30 10:38:25 +0530

    Day 1 and Day 2.

---
+++ week1/day1.py
@@ -0,0 +1,34 @@
+""" 
+Today's Mission:
+
+# Create variables for your name, age, and favorite food
+
+# Print them in a sentence
+
+# Do 5 different math operations
+
+"""
+name="Clifford"
+age=23
+fav_food="Mac&Cheese"
+
+print(f'Hola, {name}.') #formatted string Literals (f-string)
+print(f'you are {age} years old.')
+print(f'I love {fav_food}.')
+
+
+"""
+********************************************************************
+                                                                    
+  A different way to embed the value of a variable in a string       
+  print("Hola, {}".format(name))  ***********str.format()********** 
+                                                                    
+********************************************************************
+"""
+
+
+print("2 + 2 =", 2 + 2)
+print("10 / 3 =", 10 / 3)
+print("2 - 2 =",2-2)
+print("10 % 2 =",10%2)
+print("10 * 2 =", 10*2)
---
+++ week1/day2.py
@@ -0,0 +1,22 @@
+#interactive programming
+name=input("What's your name?")
+age=input("What's your age?")
+
+print(f'Hello {name}')
+print(f'In 5 years time,you will be {int(age)+5} years.')
+
+# String tricks
+message = "i am learning python"
+print(message.upper())        # I AM LEARNING PYTHON
+print(message.title())        # I Am Learning Python
+
+"""mad libs game"""
+
+print("\n Mad Libs Game")
+noun=input("Give a noun? ")
+adjective=input("Give an adjective? ")
+verb=input("Give a verb? ")
+
+print(f'Once a upon there was a {noun}.')
+print(f'He was very {adjective}')
+print(f'He used to {verb} the horse')
